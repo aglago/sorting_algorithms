8e58fd885d578addda29a625118170ed416d8663
hunterxcobby <solomonsefah13@gmail.com>
2023-11-24 19:17:25 +0000

    a function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,76 +1,76 @@
 #include "sort.h"
 /**
- * counting_sort - sorting array of ints
+ * count_sort - sorting array of ints
  * @array: array of ints
  * @size: size of array
  *
  */
-void counting_sort(int *array, size_t size)
+void count_sort(int *array, size_t size)
 {
-	int *copy, *counting;
-	unsigned int max_value, i, temp;
+	int *cp, *count;
+	unsigned int max, i, tmp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	i = 0;
-	max_value = 0;
+	max = 0;
 	while (i < size)
 	{
-		if (array[i] > (int)max_value)
-			max_value = array[i];
+		if (array[i] > (int)max)
+			max = array[i];
 		i++;
 	}
-	max_value++;
+	max++;
 
-	counting = malloc(max_value * sizeof(int));
-	if (counting == NULL)
+	count = malloc(max * sizeof(int));
+	if (count == NULL)
 		return;
-	copy = malloc(size * sizeof(int));
-	if (copy == NULL)
+	cp = malloc(size * sizeof(int));
+	if (cp == NULL)
 	{
-		free(counting);
+		free(count);
 		return;
 	}
 
 	i = 0;
 	while (i < size)
 	{
-		copy[i] = array[i];
+		cp[i] = array[i];
 		i++;
 	}
 
 	i = 0;
-	while (i < max_value)
+	while (i < max)
 	{
-		counting[i] = 0;
+		count[i] = 0;
 		i++;
 	}
 
 	i = 0;
 	while (i < size)
 	{
-		counting[array[i]]++;
+		count[array[i]]++;
 		i++;
 	}
 
 	i = 1;
-	while (i < max_value)
+	while (i < max)
 	{
-		counting[i] += counting[i - 1];
+		count[i] += count[i - 1];
 		i++;
 	}
 
-	print_array(counting, max_value);
+	print_array(count, max);
 
 	i = 0;
 	while (i < size)
 	{
-		temp = copy[i];
-		array[counting[temp] - 1] = temp;
-		counting[temp]--;
+		tmp = cp[i];
+		array[count[tmp] - 1] = tmp;
+		count[tmp]--;
 		i++;
 	}
-	free(counting);
-	free(copy);
+	free(count);
+	free(cp);
 }
