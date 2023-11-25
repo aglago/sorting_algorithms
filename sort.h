6bea97c1954a050a610266b36bb183a0f4980460
hunterxcobby <solomonsefah13@gmail.com>
2023-11-25 17:29:20 +0000

    header file

--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ void swap(int *a, int *b);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
